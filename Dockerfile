9e9ee6f16c2afe772ace031858f0af77b7cfe2b6
zmimgo <130599690+zmimgo@users.noreply.github.com>
2023-04-12 20:57:56 +0200

    Initial commit
    idk if this works man

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#Project dockerfile
+#If you think "Docker is cool" or if you just want to run ssisrelateddiscordbot
+#in a Docker containter, here is the file for that ;)
+#Psst! This deploys on the school's OpenShift cluser without a problem ;)
+FROM python:3.9
+# Set environment variables related to directories
+ENV SSIS_DISCORD_BOT_STORAGE_IS_FLUID=True
+ENV SSIS_DISCORD_BOT_FLUID_STORAGE_BASE_PATH=/ssis_bot_data
+COPY . /bot
+WORKDIR /bot
+# Install requirements
+RUN pip install -r requirements.txt
+# Run app
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,65 @@
+"""main.py
+Runs the bot."""
+import logging, nextcord, os
+from logging.handlers import RotatingFileHandler
+from nextcord.ext import commands  # Import command handler
+from bot_commands import (
+    clubs,
+    predefined_messages,
+    menu,
+    general,
+    pentry,
+    message_subscriptions,
+    schedules,
+    good_morning,
+    seasonal_profile_pictures,
+)  # Import bot commands
+from utils.general import LOGGING_DIRECTORY, LOGGING_HANDLER_FILEPATH
+
+# Set up logging
+logger = logging.getLogger(__name__)
+# Log handler
+if not os.path.exists(LOGGING_DIRECTORY):
+    logger.info("Logging directory does not exist. Creating...")
+    os.mkdir(LOGGING_DIRECTORY)
+    logger.info("Logging directory created.")
+# Add log filehandler
+log_filehandler = RotatingFileHandler(
+    filename=LOGGING_HANDLER_FILEPATH,
+    encoding="UTF-8",
+    maxBytes=100000,  # 100kB
+    backupCount=10,  # Keep 10 backups
+)
+# Add handler for console output
+log_console_handler = logging.StreamHandler()
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[log_filehandler, log_console_handler],
+)  # Set log level to debug and add rotating file handler
+# Get bot token
+BOT_TOKEN = os.environ.get("SSIS_BOT_TOKEN")
+intents = (
+    nextcord.Intents.all()
+)  # nextcord.Intents(messages=True, message_content=True, members=True)
+bot = commands.Bot(command_prefix="ssisb ", intents=intents)
+logger.info("Adding cogs...")
+cogs = [
+    clubs.Clubs,
+    predefined_messages.PredefinedMessages,
+    menu.Menu,
+    general.General,
+    pentry.Pentry,
+    # message_subscriptions.SubscribedMessagesSubscription,
+    # schedules.Schedules,
+    good_morning.GoodMorning,
+    seasonal_profile_pictures.SeasonalProfilePictures,
+]
+i = 1
+for cog in cogs:  # Add every cog
+    bot.add_cog(cog(bot))
+    logger.info(f"Cog {i}/{len(cogs)} added.")
+    i += 1
+logger.info("Cogs added.")
+logger.info("Logging in...")
+bot.run(BOT_TOKEN)  # Start bot and log in
